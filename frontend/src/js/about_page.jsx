a63656420505dce24b158309ff8071dcd1e465a0
Wai Hon Law <whhone@gmail.com>
2025-05-26 09:19:38 -0700

    remove personal email

--- frontend/src/js/about_page.jsx
+++ frontend/src/js/about_page.jsx
@@ -11,8 +11,6 @@ function AboutPage() {
           用圖回 message，回留言。
         </Col>
         <Col lg={6} md={6} sm={6} xs={12}>
-          <h3>聯絡方法</h3>
-          電郵：whhone@gmail.com<br/>
           Facebook：
           <a href='https://www.facebook.com/jeetoudo/'
              target='_blank' rel='noopener noreferrer'>
